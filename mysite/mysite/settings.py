a2a5a31034ebcfa1de640303c392c4bc4355f902
omarkhan5299 <mok236@nyu.edu>
2023-02-16 21:14:56 -0500

    no key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ltufbp*-6z(gmvykhm&!a-o!ml28i7y2t7^!tlxzd3uk%*#kai"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
